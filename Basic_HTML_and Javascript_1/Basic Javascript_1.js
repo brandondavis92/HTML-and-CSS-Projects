e894203bab73111d2ff6b85cae56e5bc2950806c
brandondavis92 <84476025+brandondavis92@users.noreply.github.com>
2021-07-08 22:22:13 -0700

    Update Basic Javascript_1.js

--- Basic_HTML_and Javascript_1/Basic Javascript_1.js
+++ Basic_HTML_and Javascript_1/Basic Javascript_1.js
@@ -1,6 +1,6 @@
 function My_first_function() {
     var String = "Kiss Me I'm Irish";
-    var result =  String.fontcolor("green");
+    var result =  String.fontcolor("red");
     document.getElementById("Irish").innerHTML=
     result;
 }
